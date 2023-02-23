0ff151c31bfa17226c3972799cd780a2ca4f739b
Valerii2889 <118856034+Valerii2889@users.noreply.github.com>
2023-02-23 14:04:39 +0200

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-console.log(`Hello world`);
+// console.log(`Hello world`);
 
 
 
 
 
 
+function isValidPassword(password) {
+const SAVED_PASSWORD = 'jqueryismyjam';
+  
+  // Change code below this line
+const isMatch = "mangodab3st" !== "kiwirul3z" === 'jqueryismyjam';
 
+  
+console.log(isMatch);
 
-
+  
+  // Change code above this line
+	return isMatch;
+}
 
