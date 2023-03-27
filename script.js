8b3fa54c02acaf7a34eccdee570105dc50a93956
Valerii2889 <118856034+Valerii2889@users.noreply.github.com>
2023-03-28 01:35:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,5 @@
 
 
 
-
-
-function checkPassword(password) {
-  const ADMIN_PASSWORD = 'jqueryismyjam';
-  let message;
-
-  if (password <= ADMIN_PASSWORD) { 
-   
-    // Change this line
-    message =  'Canceled by user!';
-  } else if (password === ADMIN_PASSWORD) { // Change this line
-    message = 'Welcome!';
-  } else {
-    message = 'Access denied, wrong password!';
-  }
-
-console.log(message);
-
-
-
-  
-  return message;
-}
+	const yearOfBirth = 2006;
+	console.log(yearOfBirth); // 2006
